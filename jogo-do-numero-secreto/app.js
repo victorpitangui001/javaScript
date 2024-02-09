58d03d3c6a023ec336d67a30fbde54bbb4ae9ad6
Victorpitangui001 <pitanguigit@gmail.com>
2024-02-09 19:02:28 -0300

    adicionar funcionalidade que cria lista de mumeros ja sorteados

--- jogo-do-numero-secreto/app.js
+++ jogo-do-numero-secreto/app.js
@@ -1,3 +1,5 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -33,10 +35,22 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElemtosNalista = listaDeNumerosSorteados.length;
+    if(quantidadeDeElemtosNalista == numeroLimite) {
+        listaDeNumerosSorteados = []
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
 }
 
-function limparCampo(){
+function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
