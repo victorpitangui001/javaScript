8d51f7bb0ef507882da66de21fc413ef11204a65
Victor Pitangui <pitanguigit@gmail.com>
2024-02-21 19:38:40 -0300

    adicionar o audioElement e o keyElement

--- bateriaDj/script.js
+++ bateriaDj/script.js
@@ -1,7 +1,16 @@
 document.body.addEventListener('keyup', (event) => {
-    playSound(event.code);
+    playSound(event.code.toLowerCase());
 });
 
 function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
+    if(audioElement) {
+        audioElement.play()
+    }
+
+    if(keyElement) {
+        keyElement.classList.add('active');
+    }
 }
