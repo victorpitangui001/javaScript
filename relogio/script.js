f46ba2827a1ae3d8dc435f8b6e37ce9674d5f711
Victor Pitangui <pitanguigit@gmail.com>
2024-02-23 08:04:48 -0300

    adicionar relogio digital e function fixZero

--- relogio/script.js
+++ relogio/script.js
@@ -2,3 +2,22 @@ let digitalElement = document.querySelector('.digital');
 let sElement = document.querySelector('.p_s');
 let mElement = document.querySelector('.p_m');
 let hElement = document.querySelector('.p_h');
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+}
+
+function fixZero(time) {
+    if(time < 10) {
+        return '0'+time;
+    }else {
+        return time;
+    }
+}
+
+setInterval(updateClock, 1000);
