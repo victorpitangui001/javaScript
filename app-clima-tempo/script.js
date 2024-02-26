0518abaef7cf18f5b76ecfb90c85458c5240a51c
Victor Pitangui <pitanguigit@gmail.com>
2024-02-26 10:53:31 -0300

    adicionar function showInfo

--- app-clima-tempo/script.js
+++ app-clima-tempo/script.js
@@ -1,13 +1,42 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event) => {
     event.preventDefault();
     let input = document.querySelector('#searchInput').value;
 
-    if(input !== ''){
+
+    if (input !== '') {
         showWarning('Caregando...');
-    }else {
-        
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=f7d142c0db499566851a7b5f7de7d5a1&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if (json.cod === 200) {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        } else {
+            showWarning('Não encontramos esta localização.');
+        }
+
     }
 });
+
+function showInfo(json) {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'block';
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed}<span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+}
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg;
