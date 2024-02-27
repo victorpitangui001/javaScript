441a5bb993290bb82170d34568f561c4687cf5ba
Victor Pitangui <pitanguigit@gmail.com>
2024-02-27 08:45:36 -0300

    inicio do projeto

---
+++ jogo-da-velha/script.js
@@ -0,0 +1,14 @@
+//Initial Data 
+let board = {
+    a1:'', a2:'', a3:'',
+    b1:'', b2:'', b3:'',
+    c1:'', c2:'', c3:''
+};
+let player = '';
+let warning = '';
+let playing = false;
+
+//Events
+
+
+//functions
