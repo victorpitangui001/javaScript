a2b00384eb60fdcf1197282a3074524f8d354463
Victor Pitangui <pitanguigit@gmail.com>
2024-02-27 10:34:40 -0300

    adicionar function checkWinnerFor e function isFull

--- jogo-da-velha/script.js
+++ jogo-da-velha/script.js
@@ -17,9 +17,10 @@ document.querySelectorAll('.item').forEach(item => {
 //functions
 function itemClick(event) {
     let item = event.target.getAttribute('data-item');
-    if (board[item] === '') {
+    if (playing && board[item] === '') {
         board[item] = player;
         renderBoard();
+        togglePlayer();
     }
 
 }
@@ -45,9 +46,63 @@ function renderBoard() {
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = board[i];
     }
+
+    checkGame()
 }
 
 function renderinfo() {
     document.querySelector('.vez').innerHTML = player;
     document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer() {
+    player = (player === 'x') ? 'o' : 'x';
+    renderinfo();
+}
+
+function checkGame() {
+    if (checkWinnerFor('x')) {
+        warning = 'O "x" venceu';
+        playing = false;
+    } else if (checkWinnerFor('o')) {
+        warning = 'O "o" venceu';
+        playing = false;
+    } else if (isFull()) {
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(player) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for (let w in pos) {
+        let pArray = pos[w].split(',');
+        let hasWon = pArray.every(option => board[option] === player);
+        if(hasWon) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isFull() {
+    for(let i in board) {
+        if(board[i] === '') {
+            return false;
+        }
+    }
+    return true;
 }
