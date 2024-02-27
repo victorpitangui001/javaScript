27838352f7f89460f1befb1a5eb06be80ecadf6f
Victor Pitangui <pitanguigit@gmail.com>
2024-02-27 09:51:34 -0300

    adicionar function renderBoard, renderInfo e itemClick

--- jogo-da-velha/script.js
+++ jogo-da-velha/script.js
@@ -1,8 +1,8 @@
 //Initial Data 
 let board = {
-    a1:'', a2:'', a3:'',
-    b1:'', b2:'', b3:'',
-    c1:'', c2:'', c3:''
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: ''
 };
 let player = '';
 let warning = '';
@@ -10,11 +10,44 @@ let playing = false;
 
 //Events
 document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick);
+});
 
 //functions
-function reset(){
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item');
+    if (board[item] === '') {
+        board[item] = player;
+        renderBoard();
+    }
+
+}
+
+function reset() {
     warning = '';
 
     let radom = Math.floor(Math.random() * 2);
     player = (radom === 0) ? 'x' : 'o';
+
+    for (let i in board) {
+        board[i] = '';
+    }
+
+    playing = true;
+
+    renderBoard();
+    renderinfo();
+}
+
+function renderBoard() {
+    for (let i in board) {
+        let item = document.querySelector(`div[data-item=${i}]`);
+        item.innerHTML = board[i];
+    }
+}
+
+function renderinfo() {
+    document.querySelector('.vez').innerHTML = player;
+    document.querySelector('.resultado').innerHTML = warning;
 }
